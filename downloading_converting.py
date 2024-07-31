fba156980ad6219fb95298e93ae3bf509383ea24
aligeno1720 <aligeno1720@gmail.com>
2024-07-31 00:40:16 -0700

    Add files via upload

---
+++ downloading_converting.py
@@ -0,0 +1,300 @@
+import getpass
+import time
+import socket
+from datetime import datetime
+
+
+def fname(arg):
+    pass
+
+print("dns server domain :  ")
+time.sleep(1)
+
+def login():
+    dns = input("dns:  ")
+
+if __name__ == "__main__":
+    login()
+
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+    
+print(">>>>>")
+time.sleep(2)
+print("connecting ...................")
+time.sleep(5)
+
+print(">>>>>")
+time.sleep(2)
+
+print("port")
+
+def login():
+    port = input("port: ")
+
+if __name__ == "__main__":
+    login()
+
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+
+
+print(">>>>>")
+time.sleep(2) 
+   
+print("connecting ...................")
+time.sleep(5)
+
+print(">>>>>")
+time.sleep(2)
+
+print("ssh")
+time.sleep(2)
+
+def login():
+    ssh = input("ssh: ")
+
+if __name__ == "__main__":
+    login()
+    
+print(">>>>>")
+time.sleep(5)
+print("connected ...................")
+time.sleep(1)
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+
+print("ebankingdb2.cm.com/workbench/index.do?loggedon&locale=en-cm&NavLB_DEUT= 5632.3524.8142.38")
+time.sleep(5)
+
+print("")
+time.sleep(2)
+
+print("Please Enter PIN AUTHORIZATION")
+time.sleep(1)
+def login():
+    PIN = input("PIN: ")
+
+if __name__ == "__main__":
+    login()
+    
+def fname(arg):
+    pass
+print("")
+time.sleep(2)
+
+print("SEARCHING ......... 1069-9543-G943-3214-DEUT997856743216 ")
+time.sleep(8)
+
+import math
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '█' * int(percent) + '-' * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}%", end="\r")
+            
+numbers = [x * 5 for x in range(2000, 3000)]
+results = []
+
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    results.append(math.factorial(x))
+    progress_bar(i + 1, len(numbers))
+
+
+print("CONNECTED")
+time.sleep(5)
+
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+
+print("")
+time.sleep(1)
+
+print("--------------------------------------------WELCOME TO SWIFT--------------------------------------------------------------")
+time.sleep(2)
+
+print("$ global_server/start")
+time.sleep(8)
+
+def login():
+    Login = input("Login: ")
+    Password = input("Password: ")
+
+if __name__ == "__main__":
+    login()
+    
+def fname(arg):
+    pass
+print("")
+time.sleep(8)
+print("CONNECTED")
+time.sleep(5)
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+    
+print("SWIFT-ACKS-COM-121213000490++DEUTSCHE BANK AG") 
+time.sleep(2)
+print("..........................................................................................................................")
+time.sleep(4)
+
+print("$ global_server/downloads/blockchain/converting/usdt")
+time.sleep(3)
+def login():
+    TRANSACTIONREFERENCENUMBER = input("TRANSACTION REFERENCE NUMBER: ")
+    BANKOPERATIONCODE = input("BANK OPERATION CODE: ")
+    BANKSENDER = input("BANK SENDER: ")
+    ACCOUNTNUMBERIBAN =input("ACCOUNT NUMBER/IBAN: ")
+    
+if __name__ == "__main__":
+    login()
+    
+def fname(arg):
+    pass
+print("")
+time.sleep(8)
+print("CONNECTED")
+time.sleep(1)
+
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Format the date and time as a string
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return current_time
+
+if __name__ == "__main__":
+    current_time = get_current_datetime()
+    print(f"{current_time}")
+    
+time.sleep(5)
+
+print("CURRENCY/AMOUNT   : EURO 5,137,653,123.00")
+time.sleep(8)
+
+print("-----------------------------------------------CONVERTING-----------------------------------------------------------------")
+time.sleep(12)
+import math
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '█' * int(percent) + '-' * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}%", end="\r")
+            
+numbers = [x * 5 for x in range(2000, 3000)]
+results = []
+
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    results.append(math.factorial(x))
+    progress_bar(i + 1, len(numbers))
+
+print("---------------------------------------------------100--------------------------------------------------------------------")
+time.sleep(8)
+
+print("Other addresses for www.db.com (scanned)")
+print("92.123.103.97 2a02:26f0:d8::1740:c30 2a02:26f0:d8::1740:c49r")
+time.sleep(0.1)
+print("DNS record for 92.123.103.82: a92-123-103-82.deploy.static.akamaitechnologies.com") 
+time.sleep(0.1)
+print("DNS record for 92.123.103.82: a92-123-103-82.deploy.static.akamaitechnologies.com")
+time.sleep(0.1)
+print("Discovered open port 146/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 146/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 3998/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 8899/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 500/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 6565/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 2022/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("Discovered open port 6129/tcp on 92.123.103.82")
+time.sleep(0.1)
+print("500/tcp   open  isakmp")
+time.sleep(0.1)
+print("514/tcp   open  shell")
+time.sleep(0.1)
+print("545/tcp   open  ekshell")
+time.sleep(0.1)
+print("554/tcp   open  rtsp")
+time.sleep(0.1)
+print("2042/tcp  open  csis")
+time.sleep(0.1)
+print("2048/tcp  open  dls-monitor")
+time.sleep(0.1)
+print("2049/tcp  open  nfs")
+time.sleep(0.1)
+print("2065/tcp  open  dlsrpn")
+time.sleep(0.1)
+print("2099/tcp  open  h2250-annex-g")
+time.sleep(0.1)
+print("2100/tcp  open  amiganetfs")
+time.sleep(0.1)
+print("6666/tcp  open  irc")
+time.sleep(0.1)
+print("6580/tcp  open  parsec-master")
+time.sleep(0.1)
+print("49400/tcp open  compaqdiag")
+time.sleep(0.1)
+print("correction done: 1 IP address (1 host up) scanned in 36.06 seconds")
+time.sleep(0.1)
+print("finished")
